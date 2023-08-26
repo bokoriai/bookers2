57a76e150fb6447a164575c53b584e1372c74503
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-08-26 13:03:56 +0000

    [Update,Add]book,userのindex,showページの修正、部分テンプレートファイルの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,29 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+    
   end
   
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(current_user.id)
+      redirect_to book_path(@book)
     else
+      @books = Book.all
       render :index
     end
   end
   
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = User.find(current_user.id)
   end
 
   def show
+    @book = Book.find(params[:id])
+    @books = Book.all
+    @user = @book.user
   end
 
   def edit
@@ -26,5 +34,9 @@ class BooksController < ApplicationController
   
   def destroy
   end
-    
+  
+  private
+    def book_params
+    params.require(:book).permit(:title, :body)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < ApplicationController
+ 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
   end
   
   def index
-  end
-  
-  def destroy
+    @users = User.all
+    @books = Book.all
+    @user = current_user
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:name, :introduction, :profile_image)
+    end
 end
+  
 
