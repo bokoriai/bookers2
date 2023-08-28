cbd13915cb86178e1513898986c4e46ddf322e37
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-08-28 06:01:12 +0000

    [Update]バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   
   validates :name, length: {minimum: 2,maximum: 20}, presence: true, uniqueness: true
-  validates :introduction, length: {maximum: 50}, presence: true
+  validates :introduction, length: {maximum: 50}
   
   has_many :books, dependent: :destroy
   
