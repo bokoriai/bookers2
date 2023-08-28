b3be6c4527b9f9302bebf473d6faa3075880bb6b
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-08-28 14:36:04 +0000

    [Update]rspecテストの実施、エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: "homes#top", as: "top"
+  root to: "homes#top"
   devise_for :users
-  get "homes/about"
+  get "home/about" => "homes#about"
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
   resources :users, only: [:show, :edit, :index, :update]
 end
